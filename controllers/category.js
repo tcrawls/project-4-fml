09df1c7804f2b8280fc055ca7c1020bc7d3687a5
Thompson Rawls <tcrawls@gmail.com>
2019-08-13 12:34:13 -0400

    implemented create, read and delete category functionality in controller

--- controllers/category.js
+++ controllers/category.js
@@ -12,6 +12,29 @@ categoryRouter.get('/', (req, res) => {
     })
 })
 
+categoryRouter.get('/:categoryId', (req, res) => {
+    categoryApi.getCategory(req.params.categoryId)
+        .then((category) => {
+            res.json(category)
+        })
+})
+
+categoryRouter.post('/', (req, res) => {
+    categoryApi.addNewCategory(req.body)
+        .then((category) => {
+            res.json(category)
+        })
+})
+
+categoryRouter.delete('/:categoryId', (req, res) => {
+    categoryApi.deleteCategory(req.params.categoryId)
+        .then((category) => {
+            res.json(category)
+        })
+})
+
+
+
 module.exports = {
     categoryRouter
   }
