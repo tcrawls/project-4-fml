947a326a505e7e36e1d9fbebb2ce69a4bf108afd
Thompson Rawls <tcrawls@gmail.com>
2019-08-13 12:09:31 -0400

    initial commit

---
+++ models/category.js
@@ -0,0 +1,33 @@
+const mongoose = require('./connection.js')
+
+const CategorySchema = new mongoose.Schema({
+  name: String
+})
+
+const CategoryCollection = mongoose.model('Category', CategorySchema)
+
+// CATEGORY MODEL FUNCTIONS:
+
+function getAllCategories() {
+  return CategoryCollection.find()
+}
+
+function getCategory(categoryId) {
+  return CategoryCollection.findById(categoryId)
+}
+
+function addNewCategory(categoryObject) {
+  return CategoryCollection.create(categoryObject)
+}
+
+function deleteCategory(categoryId) {
+  return CategoryCollection.findByIdAndDelete(categoryId)
+}
+
+
+module.exports = {
+  getAllCategories,
+  getCategory,
+  addNewCategory,
+  deleteCategory
+}
